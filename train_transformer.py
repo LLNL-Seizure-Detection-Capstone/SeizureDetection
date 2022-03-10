0301a9debe8462d4563ae5d557d12b12c9b362ff
Sara Berrios <saramberrios@gmail.com>
2022-03-10 11:25:53 -0700

    testing transformer

--- train_transformer.py
+++ train_transformer.py
@@ -30,9 +30,39 @@ if __name__ == "__main__" :
         print('ERROR: Expected a 1 Arguement in train.py (yaml config file)')
 
 config_data = load_yaml(config_path)
-train_dl, test_dl = load_train_data(config_data)
+# train_dl, test_dl = load_train_data_(config_data)
 
-src = torch.as_strided(config_data,(sequence_size,feature_size),(1,1)).unsqueeze(1)
-target = torch.as_strided(config_data[1:],(sequence_size,feature_size),(1,1)).unsqueeze(1)
-print(target.size())
+dataset_path = config_data['dataset_path']
+chbmit_dataset = CHBMITDataset(dataset_path)
+
+length_of_dataset = chbmit_dataset.__len__() # 8190
+
+features = torch.empty(length_of_dataset) # we want to initialize empty tensors (512 x 24 x 8190)
+targets = torch.empty(length_of_dataset) # (1 x 8190)
+
+for i in range(length_of_dataset):
+    feature, target = chbmit_dataset.__getitem__(i)
+    features[i] = feature
+    targets[i] = target
+
+src = torch.as_strided(features,(sequence_size,feature_size),(1,1)).unsqueeze(1)
+target = torch.as_strided(targets,(sequence_size,feature_size),(1,1)).unsqueeze(1)
+
 # size = (sequence, batch, features)
+# tf_model= nn.Transformer(feature_size,8,2,2,2,0.2)
+
+# src_mask = tf_model.generate_square_subsequent_mask(sequence_size)
+# optimizer = torch.optim.SGD(tf_model.parameters(),lr=0.1)
+# loss_fn = torch.nn.MSELoss()
+
+# for epoch in range(1000):
+#     out = tf_model(src,target,src_mask)
+#     optimizer.zero_grad()
+#     loss = loss_fn(out,target)
+#     loss.backward()
+#     optimizer.step()
+
+# out = tf_model(src,target)
+# print(target)
+# print(out)
+
