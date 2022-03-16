051c2c9d64f4d7223a8e0be1509ca8014f4b87a2
Sara Berrios <saramberrios@gmail.com>
2022-03-16 12:46:51 -0600

    troubleshooting Transformer

--- train_transformer.py
+++ train_transformer.py
@@ -11,8 +11,28 @@ import math
 import torch.nn as nn
 import torch.nn.functional as F
 
-feature_size = 512
-sequence_size = 24
+def accuracy(y_true, y_prob):
+    y_hat = []
+    num_correct = 0
+    print(type(y_true))
+    print(type(y_hat))
+
+    # convert probabilities to binary classifier
+    for y in y_prob:
+        #print("y", y)
+        if y >= 0.5:
+            y_hat.append(1.0)
+        else:
+            y_hat.append(0.0)
+    # compare y_true to y_hat
+    for indx, y_value in enumerate(y_true):
+        if y_value == y_hat[indx]:
+            num_correct += 1
+    
+    return num_correct / len(y_true)
+
+feature_size = 24
+sequence_size = 512
 # 2 second chunk is size (512, 24)
 
 if __name__ == "__main__" :
@@ -40,14 +60,23 @@ length_of_dataset = chbmit_dataset.__len__() # 8190
 features = torch.empty(size=(8190, 512, 24)) # we want to initialize empty tensors (512 x 24 x 8190)
 targets = torch.empty(length_of_dataset) # (1 x 8190)
 
+count = 0
 for i in range(length_of_dataset):
     feature, target = chbmit_dataset.__getitem__(i)
     features[i] = feature
     targets[i] = target
 
-
 src = torch.as_strided(features,(sequence_size,feature_size),(1,1)).unsqueeze(1)
-target = torch.as_strided(targets,(sequence_size,feature_size),(1,1)).unsqueeze(1)
+
+target = torch.as_strided(targets,(sequence_size,feature_size),(1,1)).unsqueeze(1) # target no longer contains any 1 values (tensor full of zeroes)
+# as strided seems necessary because we want target to be of size (T, E) where T is target sequence length and E is the feature number
+# however when we covert our list of 8190 targets we get a 512 x 1 x 24 tensor full of zeroes
+
+print("TARGET AFTER AS_STRIDED:\n", target)
+print("Any 1's:", torch.any(target))
+print("SUM TARGET:", torch.sum(target))
+print("SUM SOURCE:", torch.sum(src))
+
 
 # size = (sequence, batch, features)
 tf_model= nn.Transformer(feature_size,8,2,2,2,0.2)
@@ -58,16 +87,28 @@ loss_fn = torch.nn.MSELoss()
 binary_loss_fn = nn.BCELoss()
 # try using binary cross entropy
 
-for epoch in range(1000):
+#train_acc = 0
+total_loss = 0
+
+for epoch in range(100):
     out = tf_model(src,target,src_mask)
     optimizer.zero_grad()
     out = torch.sigmoid(out) # run outputs through a sigmoid to be between (0, 1)
     loss = binary_loss_fn(out,target)
+    total_loss += loss.item()
     loss.backward()
     optimizer.step()
+    #curr_epoch_acc = flat_accuracy()
+    #train_acc += curr_epoch_acc
+
+    avg_train_loss = total_loss / len(out) 
+    print("Average training loss:{0:.2f}".format(avg_train_loss))
 
 out = tf_model(src,target)
-sig_out = torch.sigmoid(out) # check out various thresholds (precision and recall)
-print(target)
-print(sig_out)
+# print("output shape:", out.size())
+# sig_out = torch.sigmoid(out) # check out various thresholds (precision and recall) (24 x 1 x 512)
+# print("sigmoid output shape:", sig_out.size())
+# # print(target)
+# print("OUPUT VALUES:\n", sig_out)
+# #print("Accuracy: ", accuracy(targets, sig_out))
 
